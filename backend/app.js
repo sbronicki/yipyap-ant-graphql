bfb7a2d357f39106b7e0f94400e37d9ccc663f1d
sbronicki <sbronicki@gmail.com>
2022-01-25 23:12:18 -0500

    connected to yipyap db, app and .env gitignored

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,18 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+
+// const pw = process.env.DB_PW;
+
+mongoose.connect(
+  // `mongodb+srv://stanley:${process.env.DB_PW}@cluster0.llbrq.mongodb.net/node-react-yipyap?retryWrites=true&w=majority`
+  `mongodb+srv://stanley:DGzZWdY3brfWGzQI@cluster0.llbrq.mongodb.net/node-react-yipyap?retryWrites=true&w=majority`
+);
+mongoose.connection.once("open", () => {
+  console.log("Connected to DB");
+});
 
 const schema = require("./schema/schema");
 
