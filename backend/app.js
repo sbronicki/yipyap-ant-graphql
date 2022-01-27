f2b0397ca4a46a6f1529b86d77fbd1326f4720d3
sbronicki <sbronicki@gmail.com>
2022-01-26 22:52:50 -0500

    cors

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,22 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
-require("dotenv").config();
-
 const mongoose = require("mongoose");
-
-const pw = process.env.DB_PW;
-
-mongoose.connect(
-  `mongodb+srv://stanley:${process.env.DB_PW}@cluster0.llbrq.mongodb.net/node-react-yipyap?retryWrites=true&w=majority`
-);
-mongoose.connection.once("open", () => {
-  console.log("Connected to DB");
-});
+const cors = require("cors");
+require("dotenv").config();
 
 const schema = require("./schema/schema");
 
 const app = express();
+
+mongoose.connect(
+  `mongodb+srv://stanley:${process.env.DB_PW}@cluster0.llbrq.mongodb.net/node-react-yipyap?retryWrites=true&w=majority`
+);
+
+mongoose.connection.once("open", () => {
+  console.log("Connected to DB");
+});
+
+app.use(cors());
 
 app.use(
   "/graphql",
