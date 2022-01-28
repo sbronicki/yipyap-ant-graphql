3b80b041badd5999d5a76291b5266ebf069a7d18
sbronicki <sbronicki@gmail.com>
2022-01-27 19:39:16 -0500

    axios

--- backend/app.js
+++ backend/app.js
@@ -8,9 +8,13 @@ const schema = require("./schema/schema");
 
 const app = express();
 
-mongoose.connect(
-  `mongodb+srv://stanley:${process.env.DB_PW}@cluster0.llbrq.mongodb.net/node-react-yipyap?retryWrites=true&w=majority`
-);
+mongoose
+  .connect(
+    `mongodb+srv://stanley:${process.env.DB_PW}@cluster0.llbrq.mongodb.net/node-react-yipyap?retryWrites=true&w=majority`
+  )
+  .catch(() => {
+    console.log("Connnection failed...");
+  });
 
 mongoose.connection.once("open", () => {
   console.log("Connected to DB");
