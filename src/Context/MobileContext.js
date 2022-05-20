d46ba2b9d2ff0d11969e307ecd8108b9a6537827
sbronicki <sbronicki@gmail.com>
2022-05-19 23:01:50 -0400

    mobile context logic fix, just needs debounce

--- src/Context/MobileContext.js
+++ src/Context/MobileContext.js
@@ -3,19 +3,26 @@ import React, { createContext, useEffect, useState } from "react";
 export const MobileContext = createContext(null);
 
 export const MobileProvider = ({ children }) => {
-  const [isMobile, setIsMobile] = useState();
-  window.gl_isMobile = isMobile;
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [isMobile, setIsMobile] = useState(windowWidth < 768);
 
-  const onResize = () => {
-    const _isMobile = window.innerWidth < 768;
-    if ((_isMobile && !isMobile) || (!_isMobile && isMobile)) {
-      setIsMobile(_isMobile);
-    }
+  const onResize = ({ target }) => {
+    setWindowWidth(target.innerWidth);
   };
 
   useEffect(() => {
     window.addEventListener("resize", onResize);
   }, []);
+
+  useEffect(() => {
+    const _isMobile = windowWidth < 768;
+    if ((_isMobile && !isMobile) || (!_isMobile && isMobile)) {
+      setIsMobile(_isMobile);
+    }
+  }, [windowWidth]);
+
+  window.gl_isMobile = isMobile;
+  window.gl_innerWidth = windowWidth;
 
   return (
     <MobileContext.Provider value={{ isMobile: isMobile }}>
